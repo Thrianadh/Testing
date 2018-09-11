6f52a00a42a2d318b3b33c6720e091f6e5d9939e
ThrinadhN <thrinadhn@gmail.com>
2018-09-11 13:26:04 +0100

    Commit message add index file for testing

---
+++ 02.py
@@ -0,0 +1,6 @@
+# A string is a textual data value #
+### Literal and end with Quate marks ###
+### contain the text between the quate marks ###
+### can use single quote or double quote marks as delimters,but not both on the same string expression ###
+
+"she's is cool"
