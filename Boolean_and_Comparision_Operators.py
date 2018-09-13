5f25c35af20afd44e773c25160c761b20b9dcc23
ThrinadhN <thrinadhn@gmail.com>
2018-09-13 10:35:35 +0100

    Boolean and Comparisions Operators

---
+++ Boolean_and_Comparision_Operators.py
@@ -0,0 +1,67 @@
+# Comparision operators 
+# <,>,>=,<=,==,!=
+# Can be used in chain comparision operations
+
+
+
+# Membership operator
+### The IN statement checks if a variable is also found in another variable.
+
+# Boolean operators
+### True.  Numerical substitute is 1
+### False. Numerical substitute is 0
+print(True)
+
+print(False)
+
+print(True==0)
+
+print(True==1)
+
+print(False==1)
+
+print(False==0)
+
+# Inverse Boolean
+### The NOT statement can be used to create an inverse boolean output.
+print(not True)
+print(not False)
+
+
+
+print( True  and True )
+print( True  and False)
+print( False and True )
+print( False and False)
+print( True  or  True )
+print( True  or  False)
+print( False or  True )
+print( False or  False)
+
+
+print(10 == 10)
+print(10 != 10)
+print(10 != 2 )
+print(50 == 2 )
+
+print(10>1 and 1>10)
+print(10>1 and 4!=4)
+
+var1="house";
+var2="cinema";
+var3="temple"
+
+print("o" in var1 )
+print("g" in var2 )
+print("t" in var3 )
+
+# chain Comparision operator
+print(24>=20>4==4 and 50<100>40 )
+print(24>=20>4==4 and 50<100>40!=40)
+
+print("h" in var1 and "z" not in var3)
+print("h" in var1 and "z" not in var3 and 10>4!=50 and 4000<=5000)
+print("h" in var1 or "z" not in var3 and 10>4!=50 and 4000<=5000)
+
+
+print(var1,var2,var3)
